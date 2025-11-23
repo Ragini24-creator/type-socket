1dac44f4a8103c5dcbe3607bddaf0f6a8337c2d7
Ragini Kishor <raginikishor5603@gmail.com>
2025-11-23 20:33:13 +0530

    feat(server): integrate Websocket server with typed EventRouter

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -22,9 +22,18 @@ wss.on("connection",(ws)=>{
   console.log("Client connected")
 
   ws.on("message",(rawData)=>{
+    try{
      const {event,data} = JSON.parse(rawData.toString());
+
+     router.handle(event as keyof Events ,data,ws);
+
+    }catch(err){
+      console.error("Invalid message received", rawData.toString());
+    }
+
   });
 });
+
 
 
 console.log("ws running on port 8080")
