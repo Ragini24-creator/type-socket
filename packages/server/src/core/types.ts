437de0d7d2df7d2bbb5f2c21e32c760235cc502d
Ragini Kishor <raginikishor5603@gmail.com>
2025-11-27 16:32:36 +0530

    update types.ts to auto-generate Events based on zod schema

--- packages/server/src/core/types.ts
+++ packages/server/src/core/types.ts
@@ -1,5 +1,13 @@
+import {z} from "zod";
+import {schemas} from "../../schema/schema";
 
 export type Events = {
-    ping: {message: string},
-    join_room:{roomId: string},
+    [K in keyof typeof schemas] : z.infer<(typeof schemas)[K]>
 }
+
+
+// export type Events = {
+//     ping: {message: string},
+//     join_room:{roomId: string},
+// };
+
